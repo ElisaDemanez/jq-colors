33afa88fee74b3e08861c2f8c9cf106c06562aa0
Elisa Demanez <demanez.elisa@gmail.com>
2017-12-08 12:30:16 +0100

    projet final fonctionnel sans beug commenté en anglais

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// good luck !
+
+// this starts everything at the begigning
+$(document).ready( function() {
+
+// this displays each colors in each boxes 
+
+	$(".color").each(function () {
+
+		var ok = $(this).attr('data-color');
+		$(this).css('background', ok);
+		// console.log('la var ok: '+ ok);
+	}); 	
+
+
+
+// this launch when you click on a color square 
+
+	$(".color").click(function () {
+
+// If the ckeckbox is checked, then it'll get the color you clicked on and replace the text color 
+		if 	($('#modify-texte').is(':checked')) {
+			var ok = $(this).attr('data-color');
+			console.log('checkéé');
+			$(".main").toggle($('.main').css('color', ok));
+		}
+
+// if unchecked : change background
+		else {	
+			var bg = $(this).attr('data-color');
+			$(".main").css('background', bg);
+			console.log('la var bg: '+ bg);
+		};
+
+	});
+});
+
